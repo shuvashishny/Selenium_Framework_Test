786a794787c646d75dbd7ed8c741efdfffade01e
shuvashish <shuvashishny@gmail.com>
2019-01-19 23:30:03 -0500

    added testng.xml file

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,9 +1,11 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import pages.Page;
 
 public class BaseTest {
@@ -13,7 +15,23 @@ public class BaseTest {
     public Page page;
 
     @BeforeMethod
-    public void setUp( ) {
+    @Parameters({"browser"})
+    public void setUp(String bValue ) {
+        System.out.println("Browser is "+bValue);
+
+        String browser = bValue;
+        switch (browser.toUpperCase()) {
+            case "CHROME":
+                WebDriverManager.chromedriver().setup();
+                //Create a Chrome driver. All test classes use this.
+                driver = new ChromeDriver();
+                break;
+
+            case "FIREFOX":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+        }
         WebDriverManager.chromedriver().setup();
 
         //Create a Chrome driver. All test classes use this.
